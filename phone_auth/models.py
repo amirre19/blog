df44bd2047709c18c1aba6504769699be61080cd
amirre199 <amir.chabok18@gmail.com>
2024-01-10 17:10:31 +0330

    first commit

---
+++ phone_auth/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.shortcuts import reverse
+
+# noinspection PyUnresolvedReferences
+from phonenumber_field.modelfields import PhoneNumberField
+
+User = get_user_model()
+
+
+class PhoneNumber(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone = PhoneNumberField(unique=True, blank=False)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.phone)
+    
+
+
+class EmailAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True, blank=False)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+    
